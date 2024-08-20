a31fa93bd80815a56c2336fe9f57fcebeacba6b7
duda_cardoso <126731364+dudajosecardoso@users.noreply.github.com>
2024-08-19 21:48:35 -0300

    Create ordenarNumeros6.py

---
+++ semana2/ordenarNumeros6.py
@@ -0,0 +1,27 @@
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+    left_half = mergeSort(left_half)
+    right_half = mergeSort(right_half)
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+arr = [38, 27, 43, 3, 9, 82, 10]
+arr = mergeSort(arr)
+print(arr)  

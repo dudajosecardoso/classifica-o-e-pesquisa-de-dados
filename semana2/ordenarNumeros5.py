1c0e7d8af3d542fe0d2669a1c8bb584ceb9c7f77
duda_cardoso <126731364+dudajosecardoso@users.noreply.github.com>
2024-08-19 21:29:04 -0300

    Update ordenarNumeros5.py

--- semana2/ordenarNumeros5.py
+++ semana2/ordenarNumeros5.py
@@ -1,10 +1,10 @@
 def bubbleSort():
-    arr = [64, 34, 25, 12, 22, 11]
+    arr = [64, 22, 34, 25, 12, 22, 11]
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
-    
+
 print(bubbleSort())
